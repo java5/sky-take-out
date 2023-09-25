fdd86df4bab95789b09e70ef74718293fa29794a
java5 <swaggy@vip.qq.com>
2023-09-25 23:10:18 +0800

    公共字段自动填充代码实现

--- sky-server/src/main/java/com/sky/annotaion/AutoFill.java
+++ sky-server/src/main/java/com/sky/annotaion/AutoFill.java
@@ -12,6 +12,7 @@ import java.lang.annotation.Target;
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface AutoFill {
+
     //标记数据库操作类型：UPDATE INSERT
     OperationType value();
 
