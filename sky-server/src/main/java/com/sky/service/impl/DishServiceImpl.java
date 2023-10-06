3c445b70bfe123c8412979c869ceea4868efcfee
java5 <swaggy@vip.qq.com>
2023-10-06 17:42:49 +0800

    根据id查询菜品信息

--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -128,4 +128,26 @@ public class DishServiceImpl implements DishService {
 
 
     }
+
+
+    /**
+     * 根据id查询菜品和对应的口味数据
+     * @param id
+     * @return
+     */
+    public DishVO getByIdWithFlavor(Long id) {
+
+        //根据id查询菜品数据
+        Dish dish = dishMapper.getById(id);
+
+        //根据菜品id查询口味数据
+        List<DishFlavor> dishFlavors = dishFlavorMapper.getByDishId(id);
+
+        //将查询到的数据封装到VO
+        DishVO dishVO = new DishVO();
+        BeanUtils.copyProperties(dish,dishVO);//数据拷贝
+        dishVO.setFlavors(dishFlavors);
+
+        return dishVO;
+    }
 }
